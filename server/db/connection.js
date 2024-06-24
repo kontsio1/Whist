9f21b148444f7a5b57d3a8235ae6abee963f9f37
kontsio1 <kontsio18@gmail.com>
2024-06-24 16:35:47 +0100

    change package

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,5 +1,5 @@
-const { Pool } = require("pg");
 const {readFileSync} = require("fs");
+const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "production";
 
 require("dotenv").config({
