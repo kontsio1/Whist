31db63b03aa1a0c97ef72861d3b3e9700dd1fa88
kontsio1 <kontsio18@gmail.com>
2024-06-24 15:57:01 +0100

    testQuery

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 const {readFileSync} = require("fs");
-const ENV = process.env.NODE_ENV || "development";
+const ENV = process.env.NODE_ENV || "production";
 
 require("dotenv").config({
     path: `${__dirname}/../.env.${ENV}`,
@@ -18,12 +18,22 @@ const config =
     ENV === "production"
         ? {
             connectionString: process.env.DATABASE_URL,
-            max: 2,
             ssl: {
                 ca: readFileSync('./db/ca.crt').toString(),
             }
         }
         : {};
 const db = new Pool(config)
+
+async function testQuery() {
+    const client = await db.connect();
+    try {
+        return response = await client.query('SELECT 1');
+    } finally {
+        client.release();
+    }
+}
+
+testQuery().then(r => console.log("Test query:", r.rows[0]['?column?']));
 
 module.exports = db
