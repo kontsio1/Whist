606394a0bf87cbe775f4c3afeec5d634ce62a2bc
kontsio1 <kontsio18@gmail.com>
2024-06-24 15:37:57 +0100

    enabling cors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const {getUsers, getRoundScores, getRoundCalls, postRoundCalls, postRoundScores, postRoundTricks, getScores, postUsers,
     postRoundCall, postRoundTrick, getRoundTricks, getGameDealers, postFirstToDeal, getEndOfGameInfo
@@ -9,6 +10,7 @@ const app = express()
 
 const { port = 8080 } = process.env;
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
